b55399dd828118c9c1996d4808f1e379e57b47a5
Annas rizal <43690507+annasrizal@users.noreply.github.com>
2019-10-24 21:37:45 +0700

    shinobii

---
+++ miringGan.java
@@ -0,0 +1,15 @@
+public class miringGan{
+  public static void main(String[] args) {
+    int i;
+    int j;
+      for(i=1;i<=10;i++)
+      {
+        for(j=2;j<=i;j++)
+        {
+          System.out.print(j);
+        }
+        System.out.println();
+      }
+
+    }
+}
